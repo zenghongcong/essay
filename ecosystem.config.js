031235773b90877e8c98ea54ba87d5b65e625576
zenghongcong <zenghongcong@baogongyoucai.com>
2019-02-15 10:23:14 +0800

    pm2进程管理

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,31 @@
 module.exports = {
-  apps: [
-    {
-      name: 'essay',
-      script: 'server/index.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production: {
-        NODE_ENV: 'production'
-      }
+  apps : [{
+    name: 'vueblog',
+    script: 'build/main.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      COMMON_VARIABLE: 'true'
+    },
+    env_production: {
+      NODE_ENV: 'production'
     }
-  ],
-  deploy: {
-    production: {
-      user: 'root',
-      host: '116.196.17.78',
-      ref: 'origin/master',
-      repo: 'git@github.com:wmui/essay.git',
-      path: '/root/essay',
-      'post-deploy': 'yarn && npm run build && pm2 reload ecosystem.config.js --env production'
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '202.182.113.102',
+      ref  : 'origin/master',
+      repo : 'git@github.com:zenghongcong/essay.git',
+      path : '/var/www/essay',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
-}
+};
