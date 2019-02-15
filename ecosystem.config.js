218cf52d58c4556213a1aae7461152267c5ced98
zenghongcong <zenghongcong@baogongyoucai.com>
2019-02-15 11:37:53 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:zenghongcong/essay.git',
       path : '/var/www/essay',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
