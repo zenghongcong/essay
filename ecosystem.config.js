c276101eea5ceb7d4a5e0bb13be245d069ad0173
zenghongcong <zenghongcong@baogongyoucai.com>
2019-02-15 11:56:29 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,8 @@
 module.exports = {
   apps : [{
-    name: 'vueblog',
-    script: 'build/main.js',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
+    name: 'essay',
+    script: 'server/index.j',
     env: {
-      NODE_ENV: 'development',
       COMMON_VARIABLE: 'true'
     },
     env_production: {
